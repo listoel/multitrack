v0.01.007: Fixed sign flip issues.

examples/massless/example.py
@@ -38,7 +38,7 @@ extraction = mt.Extraction(alpha=alphazs, beta=betazs, mu=muzs,
                            dx=0, dp=0, xbump=0, pbump=0,
                            xwire=xwire, wirethickness=0.0002)
 
-k2 = 1000*mag6*np.sqrt(beta6)
+k2 = -1000*mag6*np.sqrt(beta6)
 kick = strms/1E3/betams
 septum = mt.simplems(kick, xcircms, xextrms, beta=betams)
 

examples/sps/example_dynamic_twiss.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 import sys
 
-import matplotlib.pyplot as plt
 cm = plt.cm.get_cmap('viridis')
 
 codedir = os.path.abspath('../../..')
@@ -62,7 +61,7 @@ for i, dpp_offset in enumerate(dpp_offsets):
     dx_m1 = twiss.get_value('LSE.10602','DX')/np.sqrt(beta_m1)
     dp_m1 = alpha_m1*dx_m1+twiss.get_value('LSE.10602','DPX')*np.sqrt(beta_m1)
     xbump_m1 = twiss.get_value('LSE.10602','X')/np.sqrt(beta_m1)
-    k2l_m1 = -0.5*madk2l*beta_m1**1.5
+    k2l_m1 = 0.5*madk2l*beta_m1**1.5
 
     alpha_m2 = twiss.get_value('LSE.22402','ALFX')
     beta_m2 = twiss.get_value('LSE.22402','BETX')
@@ -70,7 +69,7 @@ for i, dpp_offset in enumerate(dpp_offsets):
     dx_m2 = twiss.get_value('LSE.22402','DX')/np.sqrt(beta_m2)
     dp_m2 = alpha_m1*dx_m2+twiss.get_value('LSE.10602','DPX')*np.sqrt(beta_m2)
     xbump_m2 = twiss.get_value('LSE.22402','X')/np.sqrt(beta_m2)
-    k2l_m2 = -0.5*madk2l*beta_m2**1.5
+    k2l_m2 = 0.5*madk2l*beta_m2**1.5
 
     alpha_m3 = twiss.get_value('LSE.40602','ALFX')
     beta_m3 = twiss.get_value('LSE.40602','BETX')
@@ -78,7 +77,7 @@ for i, dpp_offset in enumerate(dpp_offsets):
     dx_m3 = twiss.get_value('LSE.40602','DX')/np.sqrt(beta_m3)
     dp_m3 = alpha_m3*dx_m3+twiss.get_value('LSE.10602','DPX')*np.sqrt(beta_m3)
     xbump_m3 = twiss.get_value('LSE.40602','X')/np.sqrt(beta_m3)
-    k2l_m3 = -0.5*madk2l*beta_m3**1.5
+    k2l_m3 = 0.5*madk2l*beta_m3**1.5
 
     alpha_m4 = twiss.get_value('LSE.52402','ALFX')
     beta_m4 = twiss.get_value('LSE.52402','BETX')
@@ -86,7 +85,7 @@ for i, dpp_offset in enumerate(dpp_offsets):
     dx_m4 = twiss.get_value('LSE.52402','DX')/np.sqrt(beta_m4)
     dp_m4 = alpha_m4*dx_m1+twiss.get_value('LSE.52402','DPX')*np.sqrt(beta_m4)
     xbump_m4 = twiss.get_value('LSE.52402','X')/np.sqrt(beta_m4)
-    k2l_m4 = -0.5*madk2l*beta_m4**1.5
+    k2l_m4 = 0.5*madk2l*beta_m4**1.5
 
     if disphack:
         dx_ex = 0

examples/sps/example_static_twiss.py
@@ -47,7 +47,7 @@ mu_m1 = 0.7476548715
 dx_m1 = 2.578448815/np.sqrt(beta_m1)
 dp_m1 = alpha_m1*dx_m1+0.04662179247*np.sqrt(beta_m1)
 xbump_m1 = -0.0005850828983/np.sqrt(beta_m1)
-k2l_m1 = -0.5*madk2l*beta_m1**1.5
+k2l_m1 = 0.5*madk2l*beta_m1**1.5
 
 alpha_m2 = -2.397587252
 beta_m2 = 99.37554046
@@ -55,7 +55,7 @@ mu_m2 = 7.405406312
 dx_m2 = 4.123013489/np.sqrt(beta_m2)
 dp_m2 = alpha_m2*dx_m2+0.09465540758*np.sqrt(beta_m2)
 xbump_m2 = -0.0006662254044/np.sqrt(beta_m2)
-k2l_m2 = -0.5*madk2l*beta_m2**1.5
+k2l_m2 = 0.5*madk2l*beta_m2**1.5
 
 alpha_m3 = -2.27071997
 beta_m3 = 99.7330477
@@ -63,7 +63,7 @@ mu_m3 = 14.06643485
 dx_m3 = 2.864494031/np.sqrt(beta_m3)
 dp_m3 = alpha_m3*dx_m3+0.04923058376*np.sqrt(beta_m3)
 xbump_m3 = -0.0008237379965/np.sqrt(beta_m3)
-k2l_m3 = -0.5*madk2l*beta_m3**1.5
+k2l_m3 = 0.5*madk2l*beta_m3**1.5
 
 alpha_m4 = -2.107756887
 beta_m4 = 89.12601545
@@ -71,7 +71,7 @@ mu_m4 = 20.74061979
 dx_m4 = 4.505432689/np.sqrt(beta_m4)
 dp_m4 = alpha_m4*dx_m4+0.1054524659*np.sqrt(beta_m4)
 xbump_m4 = 0.001451775314/np.sqrt(beta_m4)
-k2l_m4 = -0.5*madk2l*beta_m4**1.5
+k2l_m4 = 0.5*madk2l*beta_m4**1.5
 
 fig, ax = plt.subplots()
 
@@ -106,8 +106,8 @@ for i, dpp_offset in enumerate(dpp_offsets):
 
     #datproc.init_to_madx(ring, init, './out/mt/init_'+str(i)+'.madx', alpha=alpha_init, beta=beta_init, dx=dx_m1, dp=dp_m1)
 
-    dq = (0.5*(k2l_m1*dx_m1+k2l_m2*dx_m2+k2l_m3*dx_m3+k2l_m4*dx_m4)/np.pi-chroma)*dpp_offset
-    #dq = -chroma*dpp_offset
+    dq = (-0.5*(k2l_m1*dx_m1+k2l_m2*dx_m2+k2l_m3*dx_m3+k2l_m4*dx_m4)/np.pi-chroma)*dpp_offset
+    # feeddown dq and higer order multipole dq missing
 
     tracks, extractt = mt.track(ring, init, extraction=extraction,
                                 dqstart=dq, dqend=dq,
@@ -123,7 +123,6 @@ for i, dpp_offset in enumerate(dpp_offsets):
     else:
         turnind = [0 for i in extractt]
 
-    import matplotlib.pyplot as plt
     cm = plt.cm.get_cmap('viridis')
     npart = len(extractt)
     extr = [i for i in range(npart) if extractt[i]!=-1]